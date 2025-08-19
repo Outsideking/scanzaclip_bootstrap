e1a6526aa9d3daeb849c118b365d8e57e30c90a9
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 15:08:08 +0700

    Add files via upload

--- permission-control/src/permission_manager.py
+++ permission-control/src/permission_manager.py
@@ -1,1 +1,12 @@
+import argparse
 
+def test_permissions():
+    print("[permission-control] Permissions OK (stub)")
+    return 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", action="store_true")
+    args = parser.parse_args()
+    if args.test:
+        raise SystemExit(test_permissions())
